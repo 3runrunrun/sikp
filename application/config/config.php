fabf3b286a6b7f6db8f3093ed6d5fcfc543d55bf
Fathir Qisthi <3runrunrun@gmail.com>
2018-11-07 22:18:56 +0700

    change base_url in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sikp/index.php';
+$config['base_url'] = 'https://sikp.herokuapp.com/index.php';
+// $config['base_url'] = 'http://localhost/sikp/index.php';
 
 /*
 |--------------------------------------------------------------------------
